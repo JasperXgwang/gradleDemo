cc2b9bc61642b9289239c42584c4a3329a02e51f
JasperXgwang <wangxinguo@meizu.com>
2017-05-13 17:10:11 +0800

    Initial commit

---
+++ src/main/java/com/meizu/push/PushTest.java
@@ -0,0 +1,28 @@
+package com.meizu.push;
+
+import com.meizu.push.sdk.server.IFlymePush;
+import com.meizu.push.sdk.server.constant.ResultPack;
+import com.meizu.push.sdk.server.model.push.PushResult;
+import com.meizu.push.sdk.server.model.push.VarnishedMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PushTest {
+
+    public static void main(String[] args) {
+
+        IFlymePush push = new IFlymePush("secret");
+        VarnishedMessage message = new VarnishedMessage.Builder().appId(123L).build();
+        List<String> targets = new ArrayList<>();
+        targets.add("t1");
+        try {
+            ResultPack<PushResult> resultPack = push.pushMessage(message, targets);
+            System.out.println(resultPack);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
